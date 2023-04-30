06288e6df381060e13f11a9dd25de1351f006fab
Dawson Par <dawson@andrews.edu>
2023-04-30 14:23:39 -0400

    Created query 3 in query.sql

--- query.sql
+++ query.sql
@@ -6,3 +6,12 @@ SELECT g.gID, g.gLName as Lastname, g.gFName as Firstname, s.date
 FROM shift as s, guard as g
 WHERE s.gID = g.gID
 AND s.date = "2023-01-01";
+
+-- Create a query that summarizes the shifts worked by guards during a specific period of time (you
+-- decide). Display data for the Employee ID, Employee Last Name, Employee First Name, Date, Total
+-- Number of Hours, Tax %, Total Pay
+
+SELECT g.gID, g.gLName as Lastname, g.gFName as Firstname, s.date, EXTRACT(HOUR FROM (clockOutTime - clockInTime)) AS hours_worked,
+create_tax_percent(g.city, g.state) as TaxPercent, EXTRACT(HOUR FROM (clockOutTime - clockInTime)) * g.payRate as total_pay
+FROM shift as s, guard as g
+WHERE s.gID = g.gID
